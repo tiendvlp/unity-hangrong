1cb82f8a794235067890f895461c4fc58be80571
tiendvlp <tiendvlp@gmail.com>
2018-04-15 08:55:42 +0700

    add GameSparks SDK, code phần convert dữ liệu từ gsData

---
+++ TheHope/Assets/Script/NormalClass/DataInteraction/ConvertGSToObj.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using GameSparks;
+using GameSparks.Core;
+using UnityEngine;
+public class ConvertGSToObj {
+	public object fromGSData (GSData gsData) {
+		Type objType = Type.GetType (gsData.GetString ("type"));
+		object obj = Activator.CreateInstance (objType);
+		foreach (var typeField in objType.GetFields ()) {
+			if (!typeField.IsNotSerialized && gsData.ContainsKey (typeField.Name))
+
+			{
+				if (typeField.FieldType == typeof (string)) {
+					typeField.SetValue (obj, gsData.GetString (typeField.Name));
+				} else if (typeField.FieldType == typeof (int)) {
+					typeField.SetValue (obj, (int) gsData.GetNumber (typeField.Name).Value);
+				} else if (typeField.FieldType == typeof (float)) {
+					typeField.SetValue (obj, (float) gsData.GetFloat (typeField.Name).Value);
+				} else if (typeField.FieldType == typeof (bool)) {
+					typeField.SetValue (obj, gsData.GetBoolean (typeField.Name));
+				} else if (typeField.FieldType == typeof (DateTime)) {
+					typeField.SetValue (obj, gsData.GetDate (typeField.Name));
+				} else if ((typeField.FieldType == typeof (List<string>) || typeField.FieldType == typeof (string[]))) {
+					typeField.SetValue (obj, (typeField.FieldType == typeof (List<string>)) ? (object) gsData.GetStringList (typeField.Name) : gsData.GetStringList (typeField.Name).ToArray ());
+				} else if ((typeField.FieldType == typeof (List<int>) || typeField.FieldType == typeof (int[]))) {
+					typeField.SetValue (obj, (typeField.FieldType == typeof (List<int>)) ? (object) gsData.GetIntList (typeField.Name) : gsData.GetIntList (typeField.Name).ToArray ());
+				} else if ((typeField.FieldType == typeof (List<float>) || typeField.FieldType == typeof (float[]))) {
+					typeField.SetValue (obj, (typeField.FieldType == typeof (List<float>)) ? (object) gsData.GetFloatList (typeField.Name) : gsData.GetFloatList (typeField.Name).ToArray ());
+				} else if (typeField.FieldType.IsClass && !typeField.FieldType.IsGenericType && !typeField.FieldType.IsArray) {
+					typeField.SetValue (obj, fromGSData (gsData.GetGSData (typeField.Name)));
+				} else if (!typeField.FieldType.IsArray && typeof (IList).IsAssignableFrom (typeField.FieldType)) {
+					IList genericList = Activator.CreateInstance (typeField.FieldType) as IList;
+					foreach (GSData gsDataElem in gsData.GetGSDataList (typeField.Name)) {
+						object elem = fromGSData (gsDataElem);
+						genericList.Add (elem);
+					}
+					typeField.SetValue (obj, genericList);
+				} else if (typeField.FieldType.IsArray) {
+					List<GSData> gsArrayData = gsData.GetGSDataList (typeField.Name);
+					Array newArray = Array.CreateInstance (typeField.FieldType.GetElementType (), gsArrayData.Count);
+					object[] objArray = new object[gsArrayData.Count];
+					for (int i = 0; i < gsArrayData.Count; i++) {
+						objArray[i] = fromGSData (gsArrayData[i]);
+					}
+					Array.Copy (objArray, newArray, objArray.Length);
+					typeField.SetValue (obj, newArray);
+				}
+			}
+		}
+		return obj;
+	}
+
+}
---
+++ TheHope/Assets/Script/NormalClass/DataInteraction/ConvertObjToGSData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using GameSparks;
+using GameSparks.Api;
+using GameSparks.Core;
+using UnityEngine;
+
+public class ConvertObjToGSData {
+	public GSRequestData converFromObj (object obj) {
+		GSRequestData gsData = new GSRequestData ();
+		gsData.Add ("type", obj.GetType ().ToString ());
+
+		foreach (var field in obj.GetType ().GetFields ()) {
+			if (field.FieldType == typeof (string)) {
+				gsData.AddString (field.Name, field.GetValue (obj).ToString ());
+			} else if (field.FieldType == typeof (bool)) {
+				gsData.AddBoolean (field.Name, bool.Parse (field.GetValue (obj).ToString ()));
+			} else if (field.FieldType == typeof (int)) {
+				gsData.AddNumber (field.Name, (int) Convert.ToInt32 (field.GetValue (obj)));
+			} else if (field.FieldType == typeof (float) || field.GetValue (obj).GetType () == typeof (double)) {
+				gsData.AddNumber (field.Name, Double.Parse (field.GetValue (obj).ToString ()));
+			} else if (field.FieldType == typeof (List<string>) || field.GetValue (obj).GetType () == typeof (string[])) {
+				gsData.AddStringList (field.Name, (field.FieldType == typeof (List<string>)) ? field.GetValue (obj) as List<string> : new List<string> (field.GetValue (obj) as string[]));
+			} else if (field.FieldType == typeof (List<int>) || field.GetValue (obj).GetType () == typeof (int[])) {
+				gsData.AddNumberList (field.Name, (field.FieldType == typeof (List<int>)) ? field.GetValue (obj) as List<int> : new List<int> (field.GetValue (obj) as int[]));
+			} else if (field.FieldType == typeof (List<float>) || field.GetValue (obj).GetType () == typeof (float[])) {
+				gsData.AddNumberList (field.Name, (field.FieldType == typeof (List<float>)) ? field.GetValue (obj) as List<float> : new List<float> (field.GetValue (obj) as float[]));
+			} else if (field.FieldType == typeof (DateTime)) {
+				gsData.AddDate (field.Name, (DateTime) field.GetValue (obj));
+			} else if (field.FieldType.IsClass && !field.FieldType.IsGenericType && !field.FieldType.IsArray) {	
+				gsData.AddObject (field.Name, converFromObj (field.GetValue (obj)));
+			} else if (field.FieldType.IsGenericType && field.FieldType.GetGenericTypeDefinition () == typeof (List<>)) {
+				List<GSData> gsDataList = new List<GSData> ();
+				foreach (var elem in field.GetValue (obj) as IList) {
+					gsDataList.Add (converFromObj (elem));
+				}
+				gsData.AddObjectList (field.Name, gsDataList);
+			} else if (field.FieldType.IsGenericType && field.FieldType.GetGenericTypeDefinition () == typeof (List<>) || field.FieldType.IsArray) {
+				List<GSData> gsDataList = new List<GSData> ();
+				foreach (var elem in field.GetValue (obj) as IList) {
+					gsDataList.Add (converFromObj (elem));
+				}
+				gsData.AddObjectList (field.Name, gsDataList);
+			}
+		}
+		return new GSRequestData(gsData.JSON);
+	}
+}
