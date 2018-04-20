398e032508ae269d318c7fe233c0768a8c0cb773
859taisoma <phamf574@gmail.com>
2018-04-20 21:45:00 +0700

    First Pushing Of Tai

---
+++ TheHope/Assets/TestAsset/TaiFolders/Script/SDKs.cs
@@ -0,0 +1,60 @@
+﻿
+namespace Facebook.Unity
+{
+    using GameSparks.Api.Requests;
+    using System.Collections.Generic;
+    using System.Linq;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using GameSparks.Api.Responses;
+
+    public class SDKs : MonoBehaviour
+    {
+        public Text errText, nameText;
+
+        void Start()
+        {
+            FB.Init();
+        }
+
+        public void Login ()
+        {
+            FB.LogInWithReadPermissions(new List<string> { "public_profile", "email", "user_friends" }, (result) => {
+                    if (!string.IsNullOrEmpty(result.Error))
+                    {
+                        errText.text = "Err: " + result.Error.ToString();
+                        return;    
+                    }
+                    else if (result.Cancelled)
+                    {
+                        errText.text = "Err: " + result.Error.ToString();
+                        return;
+                    }
+                errText.text = "Success 1";
+                nameText.text = "n: " + result.RawResult;
+                GameSparkLogin(result.AccessToken.ToString());
+            });
+        }
+
+        public void GameSparkLogin (string accessToken)
+        {
+            new FacebookConnectRequest().SetAccessToken(accessToken).Send((response) =>
+            {
+                nameText.text = "FUCK U";
+            });
+
+            new RegistrationRequest().SetDisplayName("adminstrator").SetPassword("admin").SetUserName("admins").Send((response) =>
+            {
+                if (response.HasErrors)
+                {
+                    errText.text = "Res : " + response.Errors.ToString();
+                }
+                errText.text = "Success 3";
+                Debug.Log("ĐỤ MÁ");
+            });
+            Debug.Log("ĐỤ MÁ m");
+
+        }
+
+    }
+}
