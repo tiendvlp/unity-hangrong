72792620b2bfcbfd10b52ed5b0e6c3df39a7e446
tiendvlp <tiendvlp@gmail.com>
2018-04-24 10:32:37 +0700

    authentication

---
+++ TheHope/Assets/Script/Interface/Authentication/IAuthenticationManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IAuthenticationManager {
+    void LoginWithGS(string email, string password);
+    void LoginWithFB();
+    void Register(string email, string password);
+    void setupUserInfo(string phoneNum, string displayName, bool male);
+    void ChangePassword(string newPassword);
+    void ResetPassword(string email, string token, string newPassword);
+    void SendEmailToken(string email);
+}
