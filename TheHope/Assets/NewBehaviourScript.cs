41a2502c710ecc21e9fd36a4fecac57999effd87
tiendvlp <tiendvlp@gmail.com>
2018-04-27 17:13:02 +0700

    aa

--- TheHope/Assets/NewBehaviourScript.cs
+++ TheHope/Assets/NewBehaviourScript.cs
@@ -4,10 +4,13 @@ using System.Collections.Generic;
 using System.Data;
 using GameSparks.Core;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class NewBehaviourScript : MonoBehaviour {
 
 	void Start () {
+      DatabaseOfflineReader reader = new DatabaseOfflineReader();
+      WeaponInfo info = reader.getDataById<WeaponInfo>(1);
+      GetComponent<Text>().text = info.APower + " \n" + info.DPower+" \n"  + info.PrefabPath+" \n";
   }	
 }
 [Serializable]
